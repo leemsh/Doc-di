1b909dbabd6df92468d66d193bccd98dee0b1ef6
Luke <leemsh11@konkuk.ac.kr>
2024-07-28 00:35:51 +0900

    change to rds mysql

--- src/main/java/com/dd/server/service/MedicineService.java
+++ src/main/java/com/dd/server/service/MedicineService.java
@@ -47,14 +47,16 @@ public class MedicineService {
 
 
         // medicineResponse를 MySQL DB에 저장하기 (중복 확인하기)
-        for (MedicineResponse.Item item : medicineResponse.getBody().getItems()) {
-            Optional<Medicine> existingMedicine = medicineRepository.findByItemName(item.getItemName());
-            if (existingMedicine.isEmpty()) {
-                Medicine medicine = MedicineConverter.toEntity(item);
-                medicineRepository.save(medicine);
-                logger.info("Saved new medicine to database: {}", medicine.getItemName());
-            } else {
-                logger.info("Medicine already exists in database: {}", existingMedicine.get());
+        if(medicineResponse.getBody()!=null){
+            for (MedicineResponse.Item item : medicineResponse.getBody().getItems()) {
+                Optional<Medicine> existingMedicine = medicineRepository.findByItemName(item.getItemName());
+                if (existingMedicine.isEmpty()) {
+                    Medicine medicine = MedicineConverter.toEntity(item);
+                    medicineRepository.save(medicine);
+                    logger.info("Saved new medicine to database: {}", medicine.getItemName());
+                } else {
+                    logger.info("Medicine already exists in database: {}", existingMedicine.get());
+                }
             }
         }
 
