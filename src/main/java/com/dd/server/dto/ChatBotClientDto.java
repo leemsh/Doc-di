987012718ee124b0fd7910ab14a592dc0a1b8f4a
Luke <leemsh11@konkuk.ac.kr>
2024-09-25 00:34:43 +0900

    add some java class

---
+++ src/main/java/com/dd/server/dto/ChatBotClientDto.java
@@ -0,0 +1,10 @@
+package com.dd.server.dto;
+
+import lombok.Data;
+
+@Data
+
+public class ChatBotClientDto {
+    private String email;
+    private String message;
+}
---
+++ src/main/java/com/dd/server/service/ChatBotService.java
@@ -0,0 +1,15 @@
+package com.dd.server.service;
+
+import com.dd.server.dto.ChatBotClientDto;
+import com.dd.server.dto.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ChatBotService {
+
+    public SuccessResponse<String> chatWithRasa(ChatBotClientDto chatBotClientDto) {
+
+    }
+}
