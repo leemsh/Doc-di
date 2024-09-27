3b54fe7fc93620f509da98a9f0433d14c08cff30
Luke <leemsh11@konkuk.ac.kr>
2024-09-27 20:47:25 +0900

    change RasaDto

---
+++ src/main/java/com/dd/server/dto/RasaButtonDto.java
@@ -0,0 +1,9 @@
+package com.dd.server.dto;
+
+import lombok.Data;
+
+@Data
+public class RasaButtonDto {
+    private String payload;
+    private String title;
+}
--- src/main/java/com/dd/server/service/ChatBotService.java
+++ src/main/java/com/dd/server/service/ChatBotService.java
@@ -1,10 +1,7 @@
 package com.dd.server.service;
 
 import com.dd.server.controller.RasaController;
-import com.dd.server.dto.ChatBotClientDto;
-import com.dd.server.dto.RasaDto;
-import com.dd.server.dto.FindByMedicineChartDto;
-import com.dd.server.dto.SuccessResponse;
+import com.dd.server.dto.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -12,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Objects;
 
 
@@ -20,31 +18,23 @@ import java.util.Objects;
 public class ChatBotService {
 
     private final MedicineService medicineService;
+    private final RasaController rasaController;
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     public SuccessResponse chatWithRasa(ChatBotClientDto chatBotClientDto) {
-        RasaDto rasaDto = new RasaDto();
+        List<RasaDto> listRasaDto;
         try {
-            rasaDto = RasaController.send(chatBotClientDto).block();
+            listRasaDto = rasaController.send(chatBotClientDto).block();
         }catch (Exception e){
             logger.error(e.getMessage());
             return new SuccessResponse("Rasa API Failed", 500);
         }
-        assert rasaDto != null;
-        if(Objects.equals(rasaDto.getStatus(), 200)){
-            if(Objects.equals(rasaDto.getAction(), "DB_SEARCH")){
-                FindByMedicineChartDto findByMedicineChartDto = convertStringToMedicineChartDto(rasaDto.getData());
-                return new SuccessResponse(medicineService.getMedicine(findByMedicineChartDto),200);
-            }
-
-            // 다른 ACTION 들 생기면 추가 가능
+        assert listRasaDto != null;
 
 
-            if(Objects.equals(rasaDto.getAction(), "PLAIN")){
-                return new SuccessResponse<>(rasaDto.getData(), 200);
-            }
-        }
-        return new SuccessResponse<>("Rasa ChatBot server interner error", rasaDto.getStatus());
+        // 다른 ACTION 들 생기면 추가 가능
+
+        return new SuccessResponse<>(listRasaDto, 200);
     }
 
     private FindByMedicineChartDto convertStringToMedicineChartDto(String data) {
